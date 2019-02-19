5bffc13e457537a1d928afb0780d70ef4af73872
David Toledo <dtoledoz0207@gmail.com>
2019-02-19 12:27:32 -0600

    dinamicMarks-Added

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,12 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  marcadores:Marcador[] = [];
+
   lat: number = 51.678418;
   lng: number = 7.809007;
 
-  constructor() { }
+  constructor() { 
+    const nuevoMarcador = new Marcador(51.678418, 7.809007);
+    this.marcadores.push(nuevoMarcador);
+  }
 
   ngOnInit() {
+  }
+
+  agregarMarcador(evento){
+    const coords = evento.coords;
+
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(nuevoMarcador);
   }
 
 }
